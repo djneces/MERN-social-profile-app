04289f388dd6d26265da6c5d7be6485aeb9adead
djneces <j.necesanek@hotmail.com>
2021-04-11 17:21:53 +0200

    Add user Auth Login route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const auth = require('../../middleware/auth');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
 
@@ -19,5 +23,71 @@ router.get('/', auth, async (req, res) => {
     res.status(500).send('Server error');
   }
 });
+
+//@route POST api/auth
+//@desc Authenticate user and get token
+//@access Public
+router.post(
+  '/',
+  //validation rules in []
+  [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Please is required').exists(),
+  ],
+  async (req, res) => {
+    //check for errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      //we get this response [{},{}] if the above don't match
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      //see if user exists
+      //Model.findeOne - mongoose syntax
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        //I send back same error format like above, so we get the same msg for react
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      //compares password what user typed in and encrypted password received from the DB
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      //return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      //we take user.id, the secret from default.json and sign with jwt
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          //if no error we want to send the token
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -9,7 +9,7 @@ const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
 
-//@route GET api/users
+//@route POST api/users
 //@desc Register user
 //@access Public
 router.post(
