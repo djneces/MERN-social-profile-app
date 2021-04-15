ac7c857ed9aafb8b7fb14ccbd2105efd466bb1b3
djneces <j.necesanek@hotmail.com>
2021-04-15 17:08:15 +0200

    Add Heroku Deploy config

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -9,15 +10,21 @@ connectDB();
 //init Middleware, body parser (now is part of Express)
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.send('API running');
-});
-
 //define routes, routes from the js file, prefix to /api/
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+//serve static assets in production - under the apis
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
